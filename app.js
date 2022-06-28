c4f3b733372a865d546cb879ea78ccca439290ac
Pascal Bélanger <583452+belangp@users.noreply.github.com>
2022-06-28 10:06:00 -0400

    Create project

---
+++ app.js
@@ -0,0 +1,34 @@
+const HomeAssistant = require( 'homeassistant' );
+const Pino          = require( 'pino' );
+
+const config   = require( 'config' );
+const hass     = new HomeAssistant( config.get( 'home_assistant' ) );
+const imessage = require( 'osa-imessage' );
+const logger   = Pino();
+
+// TODO package this better
+const bridge = {
+  handleMessage: function( sender, text ) {
+    logger.info( `from ${sender}: ${text}` );
+
+    hass.services.call('test_imessage', 'script', {
+        message: text 
+    }) 
+    .catch( err => {
+      logger.error( err );
+    } );
+  }
+}
+
+imessage.listen().on( 'message', ( msg ) => {
+  if ( msg.fromMe ) {
+    return;
+  }
+
+  if ( ! config.get( 'allowed_senders' ).includes( msg.handle ) ) {
+    logger.warn( `ignoring message from unknown sender: ${msg.handle}: ${msg.text}` );
+    return;
+  }
+
+  bridge.handleMessage( msg.handle, msg.text );
+} );
